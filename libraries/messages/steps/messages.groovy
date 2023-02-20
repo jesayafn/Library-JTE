156d49cb7aa9dcf2d91a38b32cf60bfa4007ea86
Jesaya Fernando Napitupulu <52324282+jesayafn@users.noreply.github.com>
2023-02-20 16:40:39 +0700

    add messages

---
+++ libraries/messages/steps/messages.groovy
@@ -0,0 +1,8 @@
+libraries.messages.steps
+void call{
+    stage("Messaging to the my world!")
+    String nodeName = config.nodename ?: "master"
+    node(nodeName){
+        println nodeName+" on WORK!"
+    }
+}
