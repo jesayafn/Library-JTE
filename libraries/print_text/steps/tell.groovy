c010645f187a390e016eebd6d4cdf9403c867fed
Jesaya Fernando Napitupulu <52324282+jesayafn@users.noreply.github.com>
2023-02-20 18:59:01 +0700

    sa

---
+++ libraries/print_text/steps/tell.groovy
@@ -0,0 +1,9 @@
+package libraries.print_text.steps
+
+void call(){
+    stage("Messaging to the my world!")
+    String nodeName = config.nodename ?: "master"
+    node(nodeName){
+        println nodeName+" on WORK!"
+    }
+}
